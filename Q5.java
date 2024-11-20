c3640e5108c47808084e59cdd44f709c7148f96a
Scarlet76 <scarlet0ciel0@gmail.com>
2024-11-20 13:52:40 +0000

    Création Q5.java et Q5_archive_20-11-2024.txt

--- Q5.java
+++ Q5.java
@@ -1,13 +1,11 @@
 public class Q5 {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-
-        // Boucle pour afficher le triangle inversé
-        for (int i = n; i >= 1; i--) { 
-            for (int j = 0; j < i; j++) { 
+        int n = Integer.parseInt(args[0]); 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); 
+            System.out.println();
         }
     }
 }
